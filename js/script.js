46c6610316412a3e939795773a1895c69d732247
truongntv05 <truongntv05>
2026-04-16 19:58:12 +0700

    update logo

--- js/script.js
+++ js/script.js
@@ -14,3 +14,34 @@ loadComponent("about", "./components/about.html");
 loadComponent("service", "./components/service.html");
 // gọi service
 loadComponent("projects", "./components/projects.html");
+
+// search..
+
+const searchBox = document.querySelector(".search-container");
+const btn = document.getElementById("searchBtn");
+
+// click mở / search
+btn.addEventListener("click", () => {
+  if (!searchBox.classList.contains("active")) {
+    searchBox.classList.add("active");
+    searchBox.querySelector("input").focus();
+  } else {
+    const keyword = searchBox.querySelector("input").value;
+    console.log("Search:", keyword);
+  }
+});
+
+// click ngoài -> đóng
+document.addEventListener("click", (e) => {
+  if (!searchBox.contains(e.target)) {
+    searchBox.classList.remove("active");
+  }
+});
+
+// search.........................
+
+//Header đổi màu khi scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
